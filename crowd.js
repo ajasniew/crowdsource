b52fffec917f24ac28d6c697004ec88920d2e6ad
alina <alina.jasniewski@stonybrook.edu>
2016-04-02 22:49:14 -0400

    Test new link

--- crowd.js
+++ crowd.js
@@ -1,7 +1,7 @@
 console.log('crowd.js loaded');
 
 (function () { // wrapping everything within an annonymous function
-    var h = '<div class="container" align="center"><h3 align="center">How to use Crowdsourcing Platform in Computational Pathology</h3><a href="https://ajasniew.github.io/crowdsource/index.html"><img src="http://gdriv.es/cfimgs/ImageGeneral/CrowdSourcing.jpg" class="img-rounded" alt="Crowdsourcing"></a></div>';
+    var h = '<div class="container" align="center"><h3 align="center">How to use Crowdsourcing Platform in Computational Pathology</h3><a href="https://ajasniew.github.io/crowdsource/view.html"><img src="http://gdriv.es/cfimgs/ImageGeneral/CrowdSourcing.jpg" class="img-rounded" alt="Crowdsourcing"></a></div>';
    
     if (typeof (sbmApps) === 'function') { // if sbmApps library was loaded
         sbmApps.render(h);
