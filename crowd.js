f709c993357908ebb672386fb33c6f84bb27bfaf
ajasniew <alina.jasniewski@stonybrook.edu>
2016-04-09 14:35:16 -0400

    Test tooltip
    Test tooltips initialized with jQuery

--- crowd.js
+++ crowd.js
@@ -3,7 +3,9 @@ console.log('crowd.js loaded');
 (function () { // wrapping everything within an annonymous function
     var h  = '<div class="container" align="center">';
         h += '<h3>How to use Crowdsourcing Platform in Computational Pathology</h3> ';
-        h += '<a href="https://ajasniew.github.io/crowdsource/view.html"><img src="http://gdriv.es/cfimgs/ImageGeneral/CrowdSourcing.jpg"';
+        h += '<a href="#" data-toggle="tooltip" data-placement="right" title="What is crowdsourcing? ';
+        h += 'The outsourcing of tasks typically performed by a few experts to a large crowd."> ';
+        h += '<img src="http://gdriv.es/cfimgs/ImageGeneral/CrowdSourcing.jpg"';
         h += 'class="img-rounded" alt="Crowdsourcing"></a><br /><br />';
         h += '<button id="crowdButton1" type="button" class="btn btn-primary btn-block disabled">Image Analysis - Classification of Regions</button> ';
         h += '<button id="crowdButton2" type="button" class="btn btn-primary btn-block disabled">Image Analysis - Cell Nuclei Classification</button> ';
@@ -18,4 +20,10 @@ console.log('crowd.js loaded');
         dv.innerHTML = h;
         document.body.appendChild(dv);
     } 
+    
+    
+    $(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+     });
+    
 })();
