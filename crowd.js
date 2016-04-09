1b2e9ac65c1f27046e74ba5449e70441ddd8b26b
ajasniew <alina.jasniewski@stonybrook.edu>
2016-04-09 13:01:57 -0400

    Test buttons
    Test bootstrap buttons

--- crowd.js
+++ crowd.js
@@ -1,7 +1,12 @@
 console.log('crowd.js loaded');
 
 (function () { // wrapping everything within an annonymous function
-    var h = '<div class="container" align="center"><h3 align="center">How to use Crowdsourcing Platform in Computational Pathology</h3><a href="https://ajasniew.github.io/crowdsource/view.html"><img src="http://gdriv.es/cfimgs/ImageGeneral/CrowdSourcing.jpg" class="img-rounded" alt="Crowdsourcing"></a></div>';
+    var h = '<div class="container" align="center"><h3 align="center">How to use Crowdsourcing Platform in Computational Pathology</h3><a href="https://ajasniew.github.io/crowdsource/view.html"><img src="http://gdriv.es/cfimgs/ImageGeneral/CrowdSourcing.jpg" class="img-rounded" alt="Crowdsourcing"></a><br /><br />';
+        h += '<button id="crowdButton1" type="button" class="btn btn-primary btn-block disabled">Image Analysis - Classification of Regions</button><br /><br /> ';
+        h += '<button id="crowdButton2" type="button" class="btn btn-primary btn-block disabled">Image Analysis - Cell Nuclei Classification</button><br /><br /> ';
+        h += '<button id="crowdButton3" type="button" class="btn btn-primary btn-block disabled">Image Annotation - Nuclei Detection</button><br /><br /> ';
+        h += '<button id="crowdButton4" type="button" class="btn btn-primary btn-block disabled">Image Annotation - Nuclei Segmentation</button><br / ';
+        h += '</div>';
    
     if (typeof (sbmApps) === 'function') { // if sbmApps library was loaded
         sbmApps.render(h);
