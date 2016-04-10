ff9197e839036af11dc8ae64dd1e22d3879ac073
ajasniew <alina.jasniewski@stonybrook.edu>
2016-04-09 21:43:34 -0400

    Image size
    Changed image size

--- crowd.js
+++ crowd.js
@@ -6,17 +6,17 @@ console.log('crowd.js loaded');
         h += '<a href="#" data-toggle="tooltip" data-placement="right" title="What is crowdsourcing? ';
         h += 'The outsourcing of tasks typically performed by a few experts to a large crowd."> ';
         h += '<img src="http://gdriv.es/cfimgs/ImageGeneral/CrowdSourcing.jpg"';
-        h += 'class="img-thumbnail" alt="Crowdsourcing" width="660" height="300"></a><br /><br />';
+        h += 'class="img-thumbnail" alt="Crowdsourcing" width="450" height="250"></a><br /><br />';
         h += '<div class="panel-group" id="accordion"> ';
         h += '<div class="panel panel-default"><div class="panel-heading"><h5 class="panel-title"><a data-toggle="collapse" data-parent="#accordion" href="#collapse1">What is crowdsourcing?  What is CrowdFlower?</a></h5></div>' ;
         h += '<div id="collapse1" class="panel-collapse collapse in"><div class="panel-body">Crowdsourcing is the outsourcing of tasks typically performed by a few experts to a large crowd.  CrowdFlower is the crowdsourcing platform</div>';
         //start
         h += '<div class="row"><div class="col-md-4"><a href="https://ajasniew.github.io/crowdsource/images/crowdSourcing2.jpg" class="thumbnail"> ';
-        h += '<img src="https://ajasniew.github.io/crowdsource/images/crowdSourcing2.jpg" alt="Crowdsource" style="width:250px;height:200px"></a></div> ';
+        h += '<img src="https://ajasniew.github.io/crowdsource/images/crowdSourcing2.jpg" alt="Crowdsource" style="width:200px;height:150px"></a></div> ';
         h += '<div class="col-md-4"><a href="https://ajasniew.github.io/crowdsource/images/crowdSourcing3.jpg" class="thumbnail"> ';
-        h += '<img src="https://ajasniew.github.io/crowdsource/images/crowdSourcing3.jpg" alt="Crowdsource" style="width:250px;height:200px"></a></div> ';
+        h += '<img src="https://ajasniew.github.io/crowdsource/images/crowdSourcing3.jpg" alt="Crowdsource" style="width:200px;height:150px"></a></div> ';
         h += '<div class="col-md-4"><a href="https://ajasniew.github.io/crowdsource/images/crowdsource4.jpg" class="thumbnail"> ';
-        h += '<img src="https://ajasniew.github.io/crowdsource/images/crowdsource4.jpg" alt="Cinque Terre" style="width:250px;height:200px"></a></div></div></div>';  
+        h += '<img src="https://ajasniew.github.io/crowdsource/images/crowdsource4.jpg" alt="Cinque Terre" style="width:200px;height:150px"></a></div></div></div>';  
         //end
         h += '</div></div> ';
         h += '<div class="panel panel-default"><div class="panel-heading"><h5 class="panel-title"><a data-toggle="collapse" data-parent="#accordion" href="#collapse2">Image Analysis - Classification of Regions</a></h5></div> ';
